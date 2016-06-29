f9451663213793810ab54e1a1506e6618445931a
Aaron Ladage <aladage@degdigital.com>
2016-06-28 21:59:05 -0500

    Updated dependencies

--- src/accordion.js
+++ src/accordion.js
@@ -1,6 +1,5 @@
-/* */ 
-import domUtils from "domUtils";
-import objectUtils from "objectUtils";
+import domUtils from "DEGJS/domUtils";
+import objectUtils from "DEGJS/objectUtils";
 
 let accordion = function(element, options) {
 
